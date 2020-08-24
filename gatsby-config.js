c8a540644d32f031f9961f3b278b799525bd249a
rrrshtt <mrishat03@gmail.com>
2020-08-24 05:30:58 +0300

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+// eslint-disable-next-line import/no-unresolved
+const pluginConfigFactory = require('@brainhubeu/gatsby-docs-kit/plugins');
+
+module.exports = {
+  siteMetadata: {
+    title: 'Brainhub template',
+    description: 'Default template for open-source libraries',
+    image: 'https://cdn-images-1.medium.com/max/1200/1*CLUFZFaXF6NG27NA3d_JkQ.jpeg',
+    url: 'https://brainhubeu.github.io/gatsby-docs-kit/',
+    type: 'article',
+    siteName: 'Brainhub template',
+    githubUrl: 'https://github.com/brainhubeu',
+  },
+
+  // URL prefix on production environment. For more info see https://www.gatsbyjs.org/docs/path-prefix/
+  pathPrefix: process.env.PATH_PREFIX || '',
+
+  plugins: pluginConfigFactory({
+    config: `${__dirname}/gatsby-docs-kit.yml`,
+    resources: path.resolve(__dirname, './docs'),
+  }),
+};
